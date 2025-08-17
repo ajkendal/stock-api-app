9eae3ad6444f19e17f51da4d7766347280dd5d9d
Amanda J Kendal-Brown <15990675+ajkendal@users.noreply.github.com>
2025-08-17 13:32:35 -0500

    Update index.js

--- index.js
+++ index.js
@@ -44,28 +44,22 @@ async function fetchStockData() {
   try {
     const stockData = await Promise.all(
       tickersArr.map(async (ticker) => {
-        const url = `https://api.polygon.io/v2/aggs/ticker/${ticker}/range/1/day/${
-          dates.startDate
-        }/${dates.endDate}?apiKey=${import.meta.env.VITE_POLYGON_API_KEY}`;
+        const url = `https://polygon-api-worker.ajkendal-openai.workers.dev/?ticker=${ticker}&startDate=${dates.startDate}&endDate=${dates.endDate}`;
 
         const response = await fetch(url);
-        const data = await response.json();
-        const status = await response.status;
-        if (status === 200) {
-          apiMessage.innerText = 'Creating report...';
 
-          delete data.request_id;
-
-          return JSON.stringify(data);
-        } else {
-          loadingArea.innerText = 'There was an error fetching stock data.';
+        if (!response.ok) {
+          const errMsg = await response.text();
+          throw new Error('Worker error: ' + errMsg);
         }
+        apiMessage.innerText = 'Creating report...';
+        return response.text();
       })
     );
     fetchReport(stockData.join(''));
   } catch (err) {
     loadingArea.innerText = 'There was an error fetching stock data.';
-    console.error('error: ', err);
+    console.error(err.message);
   }
 }
 
@@ -99,12 +93,12 @@ async function fetchReport(data) {
       body: JSON.stringify(messages),
     });
 
+    const data = await response.json();
+
     if (!response.ok) {
       console.error(`Response Status: ${data.error}`);
       renderReport('There was an error generating the report');
     }
-
-    const data = await response.json();
 
     renderReport(data.content);
   } catch (err) {
